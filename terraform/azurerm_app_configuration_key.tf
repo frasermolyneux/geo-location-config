9941ad794a4cfbb0f31445fa46faa146e8d52776
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-03-01 20:49:24 +0000

    Adding initial key structure poc

---
+++ terraform/azurerm_app_configuration_key.tf
@@ -0,0 +1,9 @@
+resource "azurerm_app_configuration_key" "environment_keys" {
+  for_each = { for each in local.environment_keys : each.key => each }
+
+  configuration_store_id = azurerm_app_configuration.app_configuration.id
+
+  key   = each.value.key_name
+  label = each.value.label
+  value = each.value.value
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,3 +2,28 @@ locals {
   resource_group_name    = "rg-geo-location-config-${var.environment}-${var.location}-${var.instance}"
   app_configuration_name = "appcs-geo-location-${var.environment}-${var.location}-${var.instance}"
 }
+
+locals {
+  json_files = [for f in fileset("environments", "*.json") : jsondecode(file("environments/${f}"))]
+
+  environments = [for content in local.json_files : {
+    name = content.name,
+    keys = [for key in lookup(content, "keys", []) : {
+      key   = key.key,
+      label = lookup(key, "label", ""),
+      value = lookup(key, "value", "")
+    }]
+  }]
+
+  environment_keys = flatten([
+    for environment in local.environments : [
+      for key in environment.keys : {
+        key         = format("%s-%s", environment.name, key.key)
+        environment = environment.name
+        key_name    = key.key
+        label       = key.label
+        value       = key.value
+      }
+    ]
+  ])
+}
