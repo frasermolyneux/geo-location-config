6f851af9e9b46970757b5a62622465fe3b70db14
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-03-01 19:57:26 +0000

    checkov recommendations

--- terraform/azure_app_configuration.tf
+++ terraform/azure_app_configuration.tf
@@ -6,7 +6,12 @@ resource "azurerm_app_configuration" "app_configuration" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
+  purge_protection_enabled = true
+
   local_auth_enabled = false
 
   tags = var.tags
+
+  #checkov:skip=CKV_AZURE_186: Ensure App configuration encryption block is set :: CMK/encryption is not being used.
+  #checkov:skip=CKV_AZURE_188: Ensure App configuration Sku is standard :: Using free tier for cost management.
 }
